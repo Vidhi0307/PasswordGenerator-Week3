30af09c43a1e2cf5020f20da7d9bd33652a92f16
Akhilesh Sharma <vidhi.sydney07@gmail.com>
2022-08-18 01:09:30 +1000

    added arrays for all the character and also all the window prompts and confirms

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+//Homework Code starts here :
+//creating arrays of the characters to be included in the password to be generated.
+
+var NumericCharacterArray =['0','1','2','3','4','5','6','7','8','9'];
+var UpperCaseAlphaArray = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','X','Y','Z'];
+var LowerCaseAlphaArray = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','x','y','z'];
+//var SpecialCharactersArray =['!','#','$','%','&','(',')','*','+',',','-','.','/',':',';','<','=','>','?','@','[', '\','^' ,'_', ']',' ` ','{','|','}','~'];
+
 function generatePassword() {
-  // 
+  // window prompts and alerts
+
+ var passwordLength= window.prompt("How many characters would you like your password to contain?");
+  var specialChar = window.confirm("Click OK to confirm including special characters");
+  var NumChar= window.confirm("Click OK to confirm including Numeric characters");
+ var lcaseAlphabet= window.confirm("Click OK to confirm including lowercase characters");
+ var uCaseAlphabet = window.confirm("Click OK to confirm including uppercase characters");
+console.log(passwordLength);
+console.log(specialChar);
+console.log(NumChar);
+console.log(lcaseAlphabet);
+console.log(lcaseAlphabet);
+
 }
 
 // Write password to the #password input
